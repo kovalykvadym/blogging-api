058ca39d683c2ec3962e098d9da7e48c23b90a96
Vadym Kovalyk <215728571+kovalykvadym@users.noreply.github.com>
2026-05-18 00:05:01 +0300

    chore(middleware): simplify global error handler

--- src/middleware/errors/error-handler.middleware.ts
+++ src/middleware/errors/error-handler.middleware.ts
@@ -1,30 +1,13 @@
-import type { NextFunction, Request, Response } from 'express';
-import AppError from '../../utils/errors/app-error';
-import { ZodError } from 'zod';
+import type { Request, Response, NextFunction } from 'express';
 
 function errorHandler(error: unknown, _req: Request, res: Response, _next: NextFunction) {
-  const status =
-    error instanceof AppError ? error.statusCode : error instanceof ZodError ? 422 : 500;
-  const message =
-    error instanceof AppError
-      ? error.message
-      : error instanceof ZodError
-        ? 'Validation failed'
-        : 'Unknown error';
+  console.error(error);
 
-  const fields =
-    error instanceof ZodError
-      ? error.issues.map((issue) => {
-          return { field: issue.path.join(), message: issue.message };
-        })
-      : undefined;
-
-  res.status(status).json({
+  res.status(500).json({
     success: false,
     data: null,
     error: {
-      message,
-      fields,
+      message: 'Internal server error',
     },
   });
 }
